a2ad78bdaf0d272e50b80f07971a9261c882075a
Aaron Dorman <Aaron.Dorman91@gmail.com>
2024-09-12 07:24:38 -0600

    Brother HL-L2460DW Install Script

--- AddPrinter.ps1
+++ AddPrinter.ps1
@@ -3,8 +3,10 @@
 #To-Do: add command to move printer drivers to driver store
 #To-Do: add command to identify network printer to be installed
 #To-Do: add command to choose driver to be installed based on network printer
+$hostName = $env:COMPUTERNAME
+$ipAddress = Read-Host "What is your printer's IP Address?"
 pnputil /add-driver "C:\IT FILES\Drive Store\gdi\BROHL22A.INF"
-Add-PrinterDriver -Name "Brother HL-L2460DW Printer" -InfPath "C:\Windows\System32\DriverStore\FileRepository\brohl22a.inf_amd64_3aa7b3980f60ed76\BROHL22A.INF"
-Add-PrinterPort -Name "HomeLab Port" -PrinterHostAddress 192.168.0.9
-Add-Printer -DriverName "Brother HL-L2460DW Printer" -Name "Brother HL-L2460DW" -PortName "Homelab Port"
+Add-PrinterDriver -Name "$hostname" -InfPath "C:\Windows\System32\DriverStore\FileRepository\brohl22a.inf_amd64_3aa7b3980f60ed76\BROHL22A.INF"
+Add-PrinterPort -Name "Printer Port" -PrinterHostAddress $ipAddress
+Add-Printer -DriverName "Brother HL-L2460DW Printer" -Name "Brother HL-L2460DW" -PortName "Printer Port"
 Get-Printer | Format-Table
