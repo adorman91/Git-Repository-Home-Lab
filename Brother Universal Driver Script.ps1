21adac9a1bbab583681717f3c6c4326d037d49cb
Aaron Dorman <Aaron.Dorman91@gmail.com>
2024-09-12 16:49:59 -0600

    1.0 BUDS Script

---
+++ Brother Universal Driver Script.ps1
@@ -0,0 +1,11 @@
+#This script will install any Brother printer that utilizes BR-Script (PostScript language emulation).
+#Paste the code below into admin-run Powershell to execute. Printer IP Address needs to be known. 
+#MFC-9330CDW, HL-L3280, MFC-L3770CDW are supported. 
+#Full list of supported printers @: https://support.brother.com/g/b/faqend.aspx?c=us&lang=en&prod=mfcl3770cdw_us_eu_as&ftype3=2037&faqid=faq00002747_000 
+$hostName = $env:COMPUTERNAME
+$ipAddress = Read-Host "What is your printer's IP Address?"
+pnputil /add-driver "C:\IT_FILES\Drive Store\universalDriverBR-Script\brupsb0a.INF"
+Add-PrinterDriver -Name "Brother Universal Printer (BR-Script3)" -InfPath 'C:\Windows\System32\DriverStore\FileRepository\brupsb0a.inf_amd64_18a5605c544df2e2\brupsb0a.INF'
+Add-PrinterPort -Name "Printer Port" -PrinterHostAddress $ipAddress
+Add-Printer -DriverName "Brother Universal Printer (BR-Script3)" -Name "Brother Universal Printer" -PortName "Printer Port"
+Get-Printer | Format-Table
